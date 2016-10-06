caf5a21264097f39ea20569826f0fd267416b3c6
marc <474733787@qq.com>
2016-10-06 11:03:10 +0800

    //complete version //score computation: whether the feature shows up.

--- aspam.h
+++ aspam.h
@@ -9,6 +9,7 @@
 #include "pcre.h"
 #include <cmath>
 #include <set>
+#include <queue>
 
 namespace aspam
 {
@@ -27,10 +28,20 @@ namespace aspam
 		const int ovec_offset=1;
 		const int BKDR_size=501;
 		const float thres_thickness=0.05;
-		const int ensemble_num=5;
+		const int ensemble_num=25;
 		const std::string classifier_root="weights";
+		const std::string ft_root="ft_set";
 		const float init_base_weight=1.0;
 		const float spam_thres=0.5;
+		const int k=5;
+		const int L=5;
+		const int seg_len=21;
+		const int LSH_vol=501;
+		const std::string LSH_root="LSH";
+		const int spam_idx_factor=1;
+		const int ham_idx_factor=-1;
+		const int nn_num=31;
+		const float sample_prob=0.68;
 
 	}
 
@@ -91,6 +102,9 @@ namespace aspam
 
 		std::vector<std::list<pair> > arr;
 
+		
+
+
 		bool is_existing(const std::string& x)
 		{
 			int idx=Hash(x.c_str());
@@ -127,6 +141,29 @@ namespace aspam
 						return &(*it);
 				
 				arr[idx].push_front(pair(x,init_val));
+				num++;
+				return &(*(arr[idx].begin()));
+			}
+		}
+
+		pair* find(const std::string& x,T init)
+		{
+			int idx=Hash(x.c_str());
+			if(arr[idx].empty())
+			{
+				arr[idx].push_back(pair(x,init));
+				num++;
+				return &(*(arr[idx].begin()));
+			}
+			else
+			{
+				std::list<pair>::iterator it;
+				for(it=arr[idx].begin();it!=arr[idx].end();
+					it++)
+					if(it->key==x)
+						return &(*it);
+				
+				arr[idx].push_front(pair(x,init));
 				num++;
 				return &(*(arr[idx].begin()));
 			}
@@ -226,7 +263,59 @@ namespace aspam
 			
 			fclose(fp);
 		}
+
+		float product(BKDR<float>& x) const
+		{
+			float res=0;
+			for(int m=0;m<this->volume;m++)
+			{
+				std::list<BKDR::pair>::const_iterator it;
+				for(it=this->arr[m].begin();it!=this->arr[m].end();
+					it++)
+					//res+=x[it->key]*(it->val);
+					res+=fast_product(it->val,x[it->key]);
+			}
+			return res;
+		}
+		float product(BKDR<int>& x) const
+		{
+			float res=0;
+			for(int m=0;m<this->volume;m++)
+			{
+				std::list<BKDR::pair>::const_iterator it;
+				for(it=this->arr[m].begin();it!=this->arr[m].end();
+					it++)
+					res+=x[it->key]*(it->val);
+			}
+			return res;
+		}
+
+		float dis_sq(BKDR<int>& x) const
+		{
+			float res=0;
+			for(int m=0;m<this->volume;m++)
+			{
+				std::list<BKDR::pair>::const_iterator it;
+				for(it=this->arr[m].begin();it!=this->arr[m].end();
+					it++)
+				{
+					float val=(x[it->key]-(it->val));
+					res+=val*val;
+				}
+			}
+			return res;
+		}
+
 	protected:
+
+		float fast_product(int x,float y) const
+		{
+			float res=0;
+			for(int i=0;i<x;i++)
+				res+=y;
+			return res;
+		}
+
 		void extract(const std::string& cnt)
 		{
 			pcre* re;
@@ -437,51 +526,340 @@ namespace aspam
 	};
 
 
+
+	
+
 	class LSH
 	{
 	public:
-		BKDR<float>& generate_v(std::set<std::string> attr_set)
+
+		class vec:public BKDR<float>
 		{
+		public:
+			float& operator[] (const std::string& x)
+			{
+				return find(x,gauss_rand())->val;
+			}
+			void print(const std::string& file_path)
+			{
+				FILE* fp;
+				fp=fopen(file_path.c_str(),"w");
+				for(int i=0;i<this->volume;i++)
+				{
+					std::list<BKDR::pair>::iterator it;
+					for(it=this->arr[i].begin();it!=this->arr[i].end();
+						it++)
+						fprintf(fp,"%s %f\n",it->key.c_str(),it->val);
+				}
+				fclose(fp);
+			}
+			void load(const std::string& file_path)
+			{
+				FILE* fp;
+				fp=fopen(file_path.c_str(),"r");
+
+				if(!fp)
+				{
+					std::cout<<"Error: cannot open "<<file_path<<"."<<std::endl;
+					return;
+				}
+				char str[10000];
+				int val;
+				while (true)
+				{
+					int res=fscanf(fp,"%s%f",str,&val);
+					if(!(res>0))
+						break;
+					(*this)[str]=val;
+				}
 			
-			std::set<std::string>::iterator it;
-			for(it=attr_set.begin();it!=attr_set.end();it++)
-				v[*it]=gauss_rand();
+				fclose(fp);
+			}
+
 			
-			return v;
+		private:
+			float gauss_rand()
+			{
+				static float V1,V2,S;
+				static int phase=0;
+				float X;
+
+				if(phase==0)
+				{
+					do
+					{
+						float U1=(float)rand()/RAND_MAX;
+						float U2=(float)rand()/RAND_MAX;
+
+						V1=2*U1-1;
+						V2=2*U2-1;
+						S=V1*V1+V2*V2;
+
+					}while(S>=1||S==0);
+
+					X=V1*sqrt(-2*log(S)/S);
+				}
+				else
+					X=V2*sqrt(-2*log(S)/S);
+
+				phase=1-phase;
+
+				return abs(X);
+			}
+			
+		};
+
+		LSH()
+		{
+			init(params::L,params::k);
+		}
+
+		void init(int L,int k)
+		{
+			vecs=std::vector<std::vector<vec > >(L);
+			bias_terms=std::vector<std::vector<float> >(L);
+			tables=std::vector<std::vector<std::list<int> > >(L);
+
+			for(int i=0;i<L;i++)
+			{
+				vecs[i]=std::vector<vec>(k);
+				bias_terms[i]=std::vector<float>(k);
+				tables[i]=std::vector<std::list<int> >(params::LSH_vol);
+
+				for(int j=0;j<k;j++)
+					bias_terms[i][j]=uni_rand();
+			}
+		}
+
+		std::vector<std::vector<int> > project(const feature& ft)
+		{
+			std::vector<std::vector<int> > res(params::L);
+			
+			for(int i=0;i<params::L;i++)
+				res[i]=std::vector<int>(params::k);
+			
+			
+			for(int i=0;i<params::L;i++)
+			{
+				for(int j=0;j<params::k;j++)
+				{
+						/*float sum=0;
+						for(int m=0;m<ft.vol();m++)
+						{
+							std::list<BKDR<int>::pair>::const_iterator it;
+							for(it=ft.arr[m].begin();it!=ft.arr[m].end();
+								it++)
+								sum+=vecs[i][j][it->key]*(it->val);
+						}*/
+						
+						//res[i][j]=(sum+bias_terms[i][j])/params::seg_len;
+						res[i][j]=(ft.product(vecs[i][j])+bias_terms[i][j])/params::seg_len;
+
+						//std::cout<<j<<std::endl;
+				}
+				
+			}
+
+			return res;
+		}
+
+		int hash(const std::vector<int>& projection)
+		{
+			int res=0;
+			unsigned int seed=131;
+			unsigned int hash=0;
+
+			std::vector<int>::const_iterator it;
+			for(it=projection.begin();it!=projection.end();
+				it++)
+				hash=hash*seed+*it;
+				
+			return (hash&0x7FFFFFFF)%params::LSH_vol;
+		}
+
+		void construct(const feature& ft,int idx)
+		{
+			std::vector<std::vector<int> > res=project(ft);
+			for(int i=0;i<params::L;i++)
+			{
+				int hash_res=hash(res[i]);
+				
+				if(ft.get_label()==params::label_spam)
+					tables[i][hash_res].push_back(params::spam_idx_factor*idx);
+				else
+					tables[i][hash_res].push_back(params::spam_idx_factor*idx);
+			}
+		}
+
+		struct pair
+		{
+			int idx;
+			float dis;
+			pair(int idx,float dis):idx(idx),dis(dis)
+			{
+			}
+			friend bool operator<(const pair& a,const pair& b)
+			{
+				  return a.dis>b.dis;
+			}               
+		};
+
+		std::set<pair> nearest_neighbors(const feature& ft,int k)
+		{
+			std::vector<std::vector<int> > proj_res=project(ft);
+			std::priority_queue<pair> heap;
+			for(int i=0;i<params::L;i++)
+			{
+				int hash_res=hash(proj_res[i]);
+				std::list<int>::const_iterator it;
+				for(it=tables[i][hash_res].begin();it!=tables[i][hash_res].end();
+					it++)
+				{
+					feature it_ft;
+					char ft_str[100];
+					sprintf(ft_str,"%s//%s//%d",params::ft_root.c_str(),
+						((*it)*params::ham_idx_factor)>0?"ham":"spam",abs(*it));
+					it_ft.load(ft_str);
+					heap.push(pair((*it),ft.dis_sq(it_ft)));
+				}
+			}
+
+			std::set<pair> res;
+			for(int i=0;i<k && !heap.empty();i++)
+			{
+				res.insert(heap.top());
+				heap.pop();
+			}
+
+			return res;
+		}
+
+		std::set<pair> nearest_neighbors(const feature& ft,int k,
+			std::map<int,feature>& ft_cache)
+		{
+			std::vector<std::vector<int> > proj_res=project(ft);
+			std::priority_queue<pair> heap;
+			for(int i=0;i<params::L;i++)
+			{
+				int hash_res=hash(proj_res[i]);
+				std::list<int>::const_iterator it;
+				for(it=tables[i][hash_res].begin();it!=tables[i][hash_res].end();
+					it++)
+				{
+					feature it_ft;
+					char ft_str[100];
+					sprintf(ft_str,"%s//%s//%d",params::ft_root.c_str(),
+						((*it)*params::ham_idx_factor)>0?"ham":"spam",abs(*it));
+					it_ft.load(ft_str);
+					
+					ft_cache[*it]=it_ft;
+
+					heap.push(pair((*it),ft.dis_sq(it_ft)));
+				}
+			}
+
+			std::set<pair> res;
+			for(int i=0;i<k && !heap.empty();i++)
+			{
+				res.insert(heap.top());
+				heap.pop();
+			}
+
+			return res;
+		}
+
+		void save()
+		{
+			FILE* fp;
+			char LSH_str[100];
+			sprintf(LSH_str,"%s//params",params::LSH_root.c_str());
+			fp=fopen(LSH_str,"w");
+			fprintf(fp,"%d %d",params::L,params::k);
+			fclose(fp);
+
+			for(int i=0;i<params::L;i++)
+				for(int j=0;j<params::k;j++)
+				{
+					sprintf(LSH_str,"%s//v_%d_%d",params::LSH_root.c_str(),i,j);
+					vecs[i][j].print(LSH_str);
+				}	
+			
+			sprintf(LSH_str,"%s//bias_terms",params::LSH_root.c_str());
+			fp=fopen(LSH_str,"w");
+			for(int i=0;i<params::L;i++)
+				for(int j=0;j<params::k;j++)
+					fprintf(fp,"%d %d %f\n",i,j,bias_terms[i][j]);
+			fclose(fp);
+			
+			sprintf(LSH_str,"%s//tables",params::LSH_root.c_str());
+			fp=fopen(LSH_str,"w");
+			for(int i=0;i<params::L;i++)
+				for(int j=0;j<params::k;j++)
+				{
+					std::list<int>::iterator it;
+					for(it=tables[i][j].begin();it!=tables[i][j].end();
+						it++)
+						fprintf(fp,"%d %d %d\n",i,j,*it);
+				}
+			fclose(fp);
+		}
+		void load()
+		{
+
+			int L;
+			int k;
+			int x;
+			int y;
+
+			FILE* fp;
+			char LSH_str[100];
+			sprintf(LSH_str,"%s//params",params::LSH_root.c_str());
+			fp=fopen(LSH_str,"r");
+			fscanf(fp,"%d%d",&L,&k);
+			fclose(fp);
+			
+			init(L,k);
+
+			for(int i=0;i<params::L;i++)
+				for(int j=0;j<params::k;j++)
+				{
+					sprintf(LSH_str,"%s//v_%d_%d",params::LSH_root.c_str(),i,j);
+					vecs[i][j].load(LSH_str);
+				}	
+			
+			sprintf(LSH_str,"%s//bias_terms",params::LSH_root.c_str());
+			fp=fopen(LSH_str,"r");
+			for(int i=0;i<params::L;i++)
+				for(int j=0;j<params::k;j++)
+					fscanf(fp,"%d%d%f",&x,&y,&bias_terms[i][j]);
+			fclose(fp);
+			
+			sprintf(LSH_str,"%s//tables",params::LSH_root.c_str());
+			fp=fopen(LSH_str,"r");
+			for(int i=0;i<params::L;i++)
+				for(int j=0;j<params::k;j++)
+				{
+					std::list<int>::iterator it;
+					for(it=tables[i][j].begin();it!=tables[i][j].end();
+						it++)
+						fscanf(fp,"%d%d%d",&x,&y,&(*it));
+				}
+			fclose(fp);
 		}
 
 	private:
-		float gauss_rand()
-		{
-			static float V1,V2,S;
-			static int phase=0;
-			float X;
-
-			if(phase==0)
-			{
-				do
-				{
-					float U1=(float)rand()/RAND_MAX;
-					float U2=(float)rand()/RAND_MAX;
-
-					V1=2*U1-1;
-					V2=2*U2-1;
-					S=V1*V1+V2*V2;
-
-				}while(S>=1||S==0);
-
-				X=V1*sqrt(-2*log(S)/S);
-			}
-			else
-				X=V2*sqrt(-2*log(S)/S);
-
-			phase=1-phase;
-
-			return X;
-		}
 		
-		BKDR<float> v;
+		std::vector<std::vector<vec > > vecs;
+		std::vector<std::vector<float> > bias_terms;
+		std::vector<std::vector<std::list<int> > > tables;
 
+		
+
+		float uni_rand()
+		{
+			float ratio=rand()/(RAND_MAX+0.0);
+			return ratio*params::seg_len;
+		}
 	};
 	
 
@@ -684,8 +1062,8 @@ namespace aspam
 				for(it=ft.arr[i].begin();it!=ft.arr[i].end();
 					it++)
 				{
-					num+=it->val;
-					score+=(it->val)*weights.get(it->key);
+					num+=it->val>0;
+					score+=(it->val>0)*weights.get(it->key);
 				}
 			}
 		}
@@ -694,22 +1072,9 @@ namespace aspam
 		{
 			float sum=0;
 			int num=0;
-			std::map<std::string,int>::const_iterator it;
-
-			for(int i=0;i<ft.vol();i++)
-			{
-				std::list<BKDR<int>::pair>::const_iterator it;
-				for(it=ft.arr[i].begin();it!=ft.arr[i].end();
-					it++)
-				{
-					num+=it->val;
-					sum+=(it->val)*weights.get(it->key);
-				}
-			}
-
 			
-
-			return sum>num?params::label_spam:params::label_ham;
+			compute_score_and_num(ft,sum,num);
+			return sum>num?params::label_spam:!params::label_spam;
 		}
 
 		
@@ -806,22 +1171,22 @@ namespace aspam
 		}
 		void save()
 		{
-				char filter_str[100];
-				for(int i=0;i<params::ensemble_num;i++)
-				{
-					sprintf(filter_str,"%s//%d",
-						params::classifier_root.c_str(),i);
-					bases[i].print(filter_str);
-				}
+			char filter_str[100];
+			for(int i=0;i<params::ensemble_num;i++)
+			{
+				sprintf(filter_str,"%s//%d",
+					params::classifier_root.c_str(),i);
+				bases[i].print(filter_str);
+			}
 
-				FILE* fp;
-				sprintf(filter_str,"%s//base_weights",
-						params::classifier_root.c_str());
-				fp=fopen(filter_str,"w");
-				fprintf(fp,"%d\n",params::ensemble_num);
-				for(int i=0;i<params::ensemble_num;i++)
-					fprintf(fp,"%d %f\n",i,bases_weights[i]);
-				fclose(fp);
+			FILE* fp;
+			sprintf(filter_str,"%s//base_weights",
+					params::classifier_root.c_str());
+			fp=fopen(filter_str,"w");
+			fprintf(fp,"%d\n",params::ensemble_num);
+			for(int i=0;i<params::ensemble_num;i++)
+				fprintf(fp,"%d %f\n",i,bases_weights[i]);
+			fclose(fp);
 		}
 		void load()
 		{
@@ -834,37 +1199,229 @@ namespace aspam
 			int ensumble_num;
 			int x;
 			fscanf(fp,"%d",&ensumble_num);
+			bases=std::vector<Winnow>(ensumble_num);
+			//bases_records=std::vector<std::set<int> >(ensumble_num);
+			bases_weights=std::vector<float>(ensumble_num);
+
 			for(int i=0;i<ensumble_num;i++)
 				fscanf(fp,"%d %f\n",&x,&bases_weights[i]);
 			fclose(fp);
 
 
-			bases=std::vector<Winnow>(ensumble_num);
-			//bases_records=std::vector<std::set<int> >(params::ensemble_num);
-			bases_weights=std::vector<float>(ensumble_num);
-
+			
 			for(int i=0;i<ensumble_num;i++)
 			{
 				sprintf(filter_str,"%s//%d",
 					params::classifier_root.c_str(),i);
+				std::cout<<"Loading base #"<<i<<std::endl;
 				bases[i].load(filter_str);
+				
 			}
+		}
+
+		void print_records()
+		{
+			FILE* fp;
+			char records_str[100];
+			sprintf(records_str,"%s//records",params::classifier_root.c_str());
+			fp=fopen(records_str,"w");
+			std::set<int>::iterator it;
+			for(it=records.begin();it!=records.end();it++)
+				fprintf(fp,"%d\n",*it);
+			fclose(fp);
+		}
+
+		void load_records()
+		{
+			FILE* fp;
+			char records_str[100];
+			sprintf(records_str,"%s//records",params::classifier_root.c_str());
+			fp=fopen(records_str,"r");
+
+			while(true)
+			{
+				int idx;
+				if(!(fscanf(fp,"%d",&idx)>0))
+					break;
+				records.insert(idx);
+			}
+			fclose(fp);
+		}
 
 		
-			
+		void test_ft(const std::string& root_spam,const std::string& root_ham,
+			int intrvl_l,int intrvl_r)
+		{
+			char ham_str[100];
+			char spam_str[100];
+
+			int num=0;
+			int ham_to_spam=0;
+			int spam_to_ham=0;
+
+			for(int i=intrvl_l;i<=intrvl_r;i++)
+			{
+				if(records.find(i)!=records.end())
+					continue;
+
+				sprintf(ham_str,"%s//%d",root_ham.c_str(),i);
+				sprintf(spam_str,"%s//%d",root_spam.c_str(),i);
+				
+				aspam::OSB ham_ft;
+				aspam::OSB spam_ft;
+
+				ham_ft.load(ham_str);
+				spam_ft.load(spam_str);
+
+				
+				if(this->is_spam(ham_ft))
+					ham_to_spam++;
+				
+				if(!this->is_spam(spam_ft))
+					spam_to_ham++;
+				
+				num++;
+
+				printf("Test #%d : %d ham_to_spam(s), %d spam_to_ham(s)\n",num,ham_to_spam,spam_to_ham);
+			}
 		}
+
+
+		void test_raw_samples(const std::string& root_spam,const std::string& root_ham,
+			int intrvl_l,int intrvl_r)
+		{
+			char ham_str[100];
+			char spam_str[100];
+
+			int num=0;
+			int ham_to_spam=0;
+			int spam_to_ham=0;
+
+			for(int i=intrvl_l;i<=intrvl_r;i++)
+			{
+				if(records.find(i)!=records.end())
+					continue;
+
+				sprintf(ham_str,"%s//%d",root_ham.c_str(),i);
+				sprintf(spam_str,"%s//%d",root_spam.c_str(),i);
+				
+				aspam::OSB ham_ft;
+				aspam::OSB spam_ft;
+
+				ham_ft.extract_and_label(ham_str,params::label_ham);
+				spam_ft.extract_and_label(spam_str,params::label_spam);
+
+				if(this->is_spam(ham_ft))
+					ham_to_spam++;
+				
+				if(!this->is_spam(spam_ft))
+					spam_to_ham++;
+
+				num++;
+				printf("Test #%d : %d ham_to_spam(s), %d spam_to_ham(s)\n",num,ham_to_spam,spam_to_ham);
+			}
+		}
+
 
 		bool is_spam(const feature& ft)
 		{
+			int ham_supporter=0;
+			int spam_supporter=0;
+			for(int i=0;i<params::ensemble_num;i++)
+			{
+				if(bases[i].is_spam(ft))
+					spam_supporter++;
+				else
+					ham_supporter++;
+			}
+			
+			return spam_supporter>ham_supporter;
+		}
+
+
+		/*bool is_spam(const feature& ft)
+		{
+			float sum=0;
+			float denom=0;
+
+
+
+			for(int i=0;i<params::ensemble_num;i++)
+			{	
+				float score;
+				int num;
+				bases[i].compute_score_and_num(ft,score,num);
+				sum+=score/num*bases_weights[i];
+			}
+			for(int i=0;i<bases_weights.size();i++)
+				denom+=bases_weights[i];
+
+			return sum/denom>1;
+		}*/
+
+		/*bool is_spam(const feature& ft)
+		{
 			float score=0;
+			float denom=0;
 			for(int i=0;i<params::ensemble_num;i++)
 				score+=bases[i].is_spam(ft)*bases_weights[i];
 			
-			return score>params::spam_thres;
+			for(int i=0;i<bases_weights.size();i++)
+				denom+=bases_weights[i];
+
+			return score/denom>params::spam_thres;
+		}*/
+		void incre_train_drift(const feature& ft)
+		{
+			std::vector<std::vector<int> > proj_res=
+				knn_finder.project(ft);
+			std::map<int,feature> ft_cache;
+
+			std::set<LSH::pair> knn_res=knn_finder.nearest_neighbors
+				(ft,params::nn_num,ft_cache);
+			
+			for(int i=0;i<bases.size();i++)
+			{
+				float weight=0;
+				float denom=0;
+				float nume=0;
+				std::set<LSH::pair>::const_iterator it;
+				for(it=knn_res.begin();it!=knn_res.end();it++)
+				{
+					denom+=it->dis;
+					int mr;
+					if(bases[i].is_spam(ft_cache[it->idx])==params::label_ham)
+					{
+						if(it->idx*params::ham_idx_factor>0)
+							mr=1;
+						else
+							mr=-1;
+					}
+					else
+					{
+						if(it->idx*params::spam_idx_factor>0)
+							mr=1;
+						else
+							mr=-1;
+					}
+					nume+=(it->dis)*mr;
+				}
+				bases_weights[i]=nume/denom;
+			}
+
+
 		}
+
 		void incre_train(const feature& ft)
 		{
+			float x=rand()/(RAND_MAX+0.0);
+			for(int i=0;i<bases.size();i++)
+				if(x>params::sample_prob)
+					continue;
+				else
+					bases[i].incre_train(ft);
 		}
+
 	private:
 		int generate_rand(int intrvl_l,int intrvl_r)
 		{
@@ -876,9 +1433,7 @@ namespace aspam
 		std::vector<std::set<int> > bases_records;
 		std::vector<float> bases_weights;
 		std::set<int> records;
-		
-
-
+		LSH knn_finder;
 	};
 	
 
--- main.cpp
+++ main.cpp
@@ -82,19 +82,89 @@ namespace cmd
 		aspam::bagging filter;
 		filter.train("data_set//spam_cnt","data_set//easy_ham_cnt",1,2500);
 		filter.save();
+		filter.print_records();
 	}
+
+	void construct_LSH()
+	{
+		aspam::LSH a;
+
+		for(int i=1;i<=2500;i++)
+		{
+			char ham_str[100];
+			char spam_str[100];
+			sprintf(ham_str,"data_set//easy_ham_cnt//%d",i);
+			sprintf(spam_str,"data_set//spam_cnt//%d",i);
+		
+			aspam::OSB ham_ft;
+			aspam::OSB spam_ft;
+		
+			ham_ft.extract_and_label(ham_str,aspam::params::label_ham);
+			spam_ft.extract_and_label(spam_str,aspam::params::label_spam);
+
+			a.construct(ham_ft,i);
+			a.construct(spam_ft,i);
+			
+
+
+
+			std::cout<<i<<std::endl;
+		}
+	
+	}
+
+	void load_bagging()
+	{
+		aspam::bagging filter;
+		filter.load();
+	}
+
 }
 
 
 int main()
 {
-	
-
-
-
 	//cmd::load_filter();
 	//cmd::train();
-	cmd::train_bagging();
+	//cmd::train_bagging();
+	//cmd::construct_LSH();
+	//cmd::load_bagging();
+
+	aspam::bagging filter;
+	filter.load();
+	filter.load_records();
+	filter.test_ft("ft_set//spam","ft_set//ham",1,2500);
+	
+	/*aspam::Winnow filter;
+	char filter_str[100];
+	sprintf(filter_str,"weights//weights");
+	filter.load(filter_str);
+
+	int ham_to_spam=0;
+	int spam_to_ham=0;
+	for(int i=2500;i<=2550;i++)
+	{
+		char ham_str[100];
+		char spam_str[100];
+		sprintf(ham_str,"data_set//easy_ham_cnt//%d",i);
+		sprintf(spam_str,"data_set//spam_cnt//%d",i);
+		
+		aspam::OSB ham_ft;
+		aspam::OSB spam_ft;
+		
+		ham_ft.extract_and_label(ham_str,aspam::params::label_ham);
+		spam_ft.extract_and_label(spam_str,aspam::params::label_spam);
+
+	
+
+		if(filter.is_spam(ham_ft))
+			ham_to_spam++;
+		if(!filter.is_spam(spam_ft))
+			spam_to_ham++;
+
+		printf("%d : h_to_s %d, spam_to_ham %d\n",i,ham_to_spam,spam_to_ham);
+	}
+*/
 
 
 
